136f42d8f4aff2f8d6bd56a3f693d27266915a2a
maxall4 <maxall4@protonmail.com>
2022-08-02 20:11:07 -0700

    🎶 Music manager

--- include/sound.h
+++ include/sound.h
@@ -12,6 +12,29 @@ void SOUND_INIT();
 struct LoadedAudio {
     Mix_Chunk* audio;
 };
+
+class MusicManager {
+private:
+    std::vector<Mix_Music*> tracks;
+    int fade_time;
+    double end_time{};
+    static bool next_flag;
+    static void music_finished();
+public:
+    bool auto_next;
+    bool fade;
+    int current_track{};
+    MusicManager(bool new_fade,bool new_auto_next,const std::vector<char*>& new_tracks,int new_fade_time);
+    void next();
+    void back();
+    void resume();
+    void pause();
+    void play();
+    void add_tracks(std::vector<char *> new_tracks);
+    void cycle();
+};
+
 LoadedAudio load_wav_file(char* file_path);
+
 void play_sound(LoadedAudio audio);
 #endif //FLAME_ENGINE_SOUND_H
--- sound.cpp
+++ sound.cpp
@@ -17,6 +17,81 @@ void play_sound_looped(LoadedAudio loadedAudio) {
     Mix_PlayChannel(-1, loadedAudio.audio, -1);
 }
 
+bool MusicManager::next_flag = false;
+
+void MusicManager::music_finished() {
+    next_flag = true;
+}
+
+MusicManager::MusicManager(bool new_fade, bool new_auto_next, const std::vector<char *>& new_tracks,int new_fade_time) {
+    std::vector<Mix_Music *> load_tracks;
+    // Preload tracks
+    for(char* track: new_tracks)
+        load_tracks.push_back(Mix_LoadMUS(track));
+    // Set vars
+    MusicManager::tracks = load_tracks;
+    MusicManager::fade = new_fade;
+    MusicManager::fade_time = new_fade_time;
+    MusicManager::auto_next = new_auto_next;
+    Mix_HookMusicFinished(this->music_finished);
+}
+
+void MusicManager::cycle() {
+    if (next_flag) {
+        next_flag = false;
+        this->next();
+    }
+}
+
+void MusicManager::play() {
+    Mix_Music* current_track_chunk = tracks[current_track];
+    if (fade) {
+        Mix_FadeInMusic(current_track_chunk,0,fade_time);
+    } else {
+        Mix_PlayMusic(current_track_chunk,0);
+    }
+    // Update current track
+    current_track++;
+}
+
+void MusicManager::next() {
+    if (fade) {
+        Mix_FadeOutMusic(fade_time);
+        Mix_FadeInMusic(tracks[current_track],0,fade_time);
+        current_track++;
+    }
+}
+
+void MusicManager::add_tracks(std::vector<char *> new_tracks) {
+    std::vector<Mix_Music *> load_tracks;
+    // Preload tracks
+    for(char* track: new_tracks)
+        load_tracks.push_back(Mix_LoadMUS(track));
+    // Save
+    MusicManager::tracks = load_tracks;
+}
+
+void MusicManager::back() {
+    if (fade) {
+        Mix_FadeOutMusic(fade_time);
+        current_track -= 2;
+        Mix_FadeInMusic(tracks[current_track],0,fade_time);
+    }
+}
+
+
+void MusicManager::pause() {
+    if (fade) {
+        Mix_FadeOutMusic(fade_time);
+    } else {
+        Mix_PauseMusic();
+    }
+}
+
+void MusicManager::resume() {
+    Mix_ResumeMusic();
+}
+
 void SOUND_INIT() {
     int result = Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
     if(result < 0) {
